b3c58899c955d9c9a3ef20ed10af942d73b0080f
Andrew Lee <alee@eecs.berkeley.edu>
2013-12-06 17:14:50 -0800

    Deferred the heavy item writing

--- appengine/data.py
+++ appengine/data.py
@@ -1,5 +1,6 @@
 import cStringIO
 import csv
+from google.appengine.ext import deferred
 from google.appengine.ext import ndb
 
 # Here, field names are actually mapped to synthesized field names to prevent
@@ -9,11 +10,13 @@ class Table(ndb.Model):
   # The original filename of the table.
   filename = ndb.StringProperty(required=True)
   # The list of the original names for all the fields in the table.
-  original_fields = ndb.StringProperty(repeated=True)
+  field_names = ndb.StringProperty(repeated=True)
   # The list of names for just the numerical fields.
   number_fields = ndb.StringProperty(repeated=True)
+  # Whether or not all the items have been written.
+  all_items_written = ndb.BooleanProperty(default=False)
 
-# Item will reproduce the values in the rows.
+# Item will reproduce the values in a row.
 class Item(ndb.Expando):
   pass
 
@@ -39,14 +42,21 @@ def save_csv(filename, contents):
   for row in all_rows:
     for field in number_fields[:]:
       value = row[field]
-      if value and type(to_number(value)) is not float:
+      if value and type(to_number(value)) is str:
         number_fields.remove(field)
 
   table = Table(filename=filename,
-                original_fields=field_names,
+                field_names=field_names,
                 number_fields=number_fields)
   table.put()
 
+  deferred.defer(deferred_save_rows, table, all_rows)
+
+  return table.key.integer_id()
+
+def deferred_save_rows(table, all_rows):
+  field_names = table.field_names
+  number_fields = table.number_fields
   items = []
   for row in all_rows:
     item = Item(parent=table.key)
@@ -64,14 +74,18 @@ def save_csv(filename, contents):
     }
     items.append(item)
   ndb.put_multi(items)
-
-  return table.key.integer_id()
+  table.all_items_written = True
+  table.put()
 
 def get_csv(data_id):
   table_key = ndb.Key(Table, data_id)
   item_query = Item.query(ancestor=table_key)
 
-  ret = table_key.get().filename
-  for item in item_query:
-    ret += '\n' + str(getattr(item, 'field0'))
+  table = table_key.get()
+  ret = table.filename
+  if table.all_items_written:
+    for item in item_query:
+      ret += '\n' + str(getattr(item, 'field0'))
+  else:
+    ret += '\nNot done processing...'
   return ret
